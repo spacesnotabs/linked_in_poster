60b1e8799cab59a6128df539563ce5adc2330a6a
spacesnotabs <christopherdavid23@gmail.com>
2025-06-20 12:24:13 -0700

    Initial setup of chat functionality

--- llm_model.py
+++ llm_model.py
@@ -1,17 +1,60 @@
 from llama_cpp import Llama
 
 class LlmModel:
-    def __init__(self, model_path: str, n_gpu_layers: int = -1, context_size: int = 2048):
+    def __init__(self, model_path: str, n_gpu_layers: int = -1, context_size: int = 32768):
         self._model = Llama(model_path=model_path, 
                             n_gpu_layers=n_gpu_layers,
                             n_ctx=context_size,
-                            verbose=True)
+                            verbose=False,
+                            chat_format='mistral-instruct')
+        self._chat_history: list[dict] = []
+        self.set_system_prompt(prompt='You are a helpful assistant.')
 
     @property
-    def model(self):
+    def model(self) -> Llama:
         return self._model
     
-    def send_prompt(prompt: str) -> str:
+    @property
+    def chat_history(self) -> list[str]:
+        return self._chat_history
+
+    def set_system_prompt(self, prompt: str) -> None:
+        system_prompt = self.create_prompt(role='system', content=prompt)
+        self.chat_history.append(system_prompt)
+
+    def clear_chat_history(self) -> None:
+        """Resets the chat history."""
+        self._chat_history = []
+
+    def create_prompt(self, role: str, content: str) -> dict:
+        """Create the prompt in the expected format."""
+        prompt={'role': role, 'content': content.strip()}
+        return prompt
+
+    def send_prompt(self, prompt: str) -> str | None:
         """Send the prompt to the LLM and return its response"""
-        return "Testing"
+        text = None
+
+        user_prompt = self.create_prompt(role='user', content=prompt)
+        self.chat_history.append(user_prompt)
+
+        try:
+            resp = self.model.create_chat_completion(
+                messages=self._chat_history,
+                max_tokens=512,
+                temperature=0.7,
+                top_p=0.9,
+                top_k=50,
+                stream=False
+            )
+
+            # print(resp)
+            text = resp['choices'][0]['message']['content']
+            assistant_prompt = self.create_prompt(role='assistant', content=text)
+            self.chat_history.append(assistant_prompt)
+
+        except Exception as e:
+            print(f"Exception resulted in sending prompt: {e}")
+
+        return text
     
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from llm_model import LlmModel
 
 if __name__ == "__main__":
-    print("loading model")
     model = LlmModel(model_path="models/mistral-7b-instruct-v0.1.Q4_K_M.gguf")
 
     while True:
@@ -9,4 +8,8 @@ if __name__ == "__main__":
         if user_input == 'exit':
             break
 
-        print(model.send_prompt(prompt=user_input))
+        chat_response = model.send_prompt(prompt=user_input)
+        if chat_response is not None:
+            print(f"Chat: {chat_response}")
+        else:
+            print("I refuse to answer!")
