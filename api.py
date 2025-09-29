028999b45de4307f8c45fb53e74ce8f225f913eb
spacesnotabs <christopherdavid23@gmail.com>
2025-09-29 15:58:04 -0700

    Minor fixes. Added utility file

--- api.py
+++ api.py
@@ -81,4 +81,4 @@ async def create_upload_file(file: UploadFile = File(...)):
     return {"filename": file.filename, "content_type": file.content_type}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
