38af72fff30dae7872c5d0a1ae80b31c3e7511bf
spacesnotabs <christopherdavid23@gmail.com>
2025-10-01 12:53:03 -0700

    Update utils.py
    Add pdf to text function

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,18 @@
+import fitz  # PyMuPDF
+
 def read_file(filename: str) -> str:
     contents: str = ""
-    with open(file=filename, mode="r", encoding="utf-8") as file:
-        contents = file.read()
+
+    if filename.lower().endswith(".pdf"):
+        contents = pdf_to_text(filename)
+    else:
+        with open(file=filename, mode="r", encoding="utf-8") as file:
+            contents = file.read()
     return contents
+
+def pdf_to_text(filepath: str) -> str:
+    text = ""
+    with fitz.open(filepath) as pdf:
+        for page in pdf:
+            text += page.get_text()
+    return text
